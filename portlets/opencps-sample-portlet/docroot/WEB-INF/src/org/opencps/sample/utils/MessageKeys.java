ec80d0a0c29859f6a1044967d6144ef446d740f7
Khoa Hạnh Phúc <khoavd@fds.vn>
2016-03-08 09:16:26 +0700

    Update OpenCPS Sample Portlet

--- portlets/opencps-sample-portlet/docroot/WEB-INF/src/org/opencps/sample/utils/MessageKeys.java
+++ portlets/opencps-sample-portlet/docroot/WEB-INF/src/org/opencps/sample/utils/MessageKeys.java
@@ -1,5 +1,4 @@
-<%
-/**
+/*******************************************************************************
  * OpenCPS is the open source Core Public Services software
  * Copyright (C) 2016-present OpenCPS community
  *
@@ -7,18 +6,24 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package org.opencps.sample.utils;
+
+/**
+ * @author trungnt
+ *
  */
-%>
-
-<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
-
-<portlet:defineObjects />
-
-This is the <b>OpenCPS Sample</b> portlet.
+public class MessageKeys {
+	public static final String _ADD_SUCCESSFUL = "add.succcess";
+	public static final String _UPDATE_SUCCESSFUL = "update.succcess";
+	public static final String _ERROR_OCCUR_UPDATE = "error.occur.update";
+	
+}
