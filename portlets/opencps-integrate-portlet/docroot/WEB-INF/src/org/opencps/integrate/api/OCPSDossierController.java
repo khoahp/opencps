caae609c5b2978d5d64cfe5771915c6d4452ba80
Khoa Vu Dang <khoavd@fds.vn>
2017-05-03 15:08:33 +0700

    UPdate API

--- portlets/opencps-integrate-portlet/docroot/WEB-INF/src/org/opencps/integrate/api/OCPSDossierController.java
+++ portlets/opencps-integrate-portlet/docroot/WEB-INF/src/org/opencps/integrate/api/OCPSDossierController.java
@@ -4,11 +4,11 @@ import java.util.Calendar;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -16,14 +16,19 @@ import javax.ws.rs.core.Response;
 
 import org.opencps.accountmgt.model.Citizen;
 import org.opencps.accountmgt.service.CitizenLocalServiceUtil;
+import org.opencps.dossiermgt.model.Dossier;
 import org.opencps.dossiermgt.model.DossierPart;
 import org.opencps.dossiermgt.model.DossierTemplate;
 import org.opencps.dossiermgt.model.ServiceConfig;
+import org.opencps.dossiermgt.service.DossierLocalServiceUtil;
 import org.opencps.dossiermgt.service.DossierPartLocalServiceUtil;
 import org.opencps.dossiermgt.service.DossierTemplateLocalServiceUtil;
 import org.opencps.dossiermgt.service.ServiceConfigLocalServiceUtil;
 import org.opencps.integrate.dao.model.IntegrateAPI;
 import org.opencps.integrate.utils.APIUtils;
+import org.opencps.integrate.utils.AccountModel;
+import org.opencps.integrate.utils.DLFolderUtil;
+import org.opencps.integrate.utils.DossierModel;
 import org.opencps.integrate.utils.DossierUtils;
 import org.opencps.servicemgt.model.ServiceInfo;
 import org.opencps.servicemgt.service.ServiceInfoLocalServiceUtil;
@@ -33,14 +38,80 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
 
 @Path("/api")
 public class OCPSDossierController {
+	
+	@POST
+	@Path("/dossiers/{dossierid}/actions")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Response submitDossier(@HeaderParam("apikey") String apikey,
+			@Context HttpServletRequest request,
+			@PathParam("dossierid") long dossierid, String body) {
+
+		JSONObject resp = JSONFactoryUtil.createJSONObject();
+
+		OCPSPermission permit = new OCPSPermission();
+
+		OCPSAuth auth = new OCPSAuth();
+
+		IntegrateAPI api = auth.auth(apikey);
+		
+
+		boolean isPermit = permit.isDossierPermission(apikey)
+				&& permit.isSendValidatorDossier(dossierid)
+				&& permit.isDossierDetailPermission(apikey, dossierid);
+
+		if (Validator.isNotNull(api)) {
+			if (isPermit) {
+				
+				try {
+					
+					User user = UserLocalServiceUtil.getUser(api.getUserId());
+					
+					JSONObject jsonMgs = JSONFactoryUtil.createJSONObject();
+					
+					Dossier dossier = DossierLocalServiceUtil.getDossier(dossierid);
+
+					jsonMgs.put("action", DossierModel.ACTION_SUBMIT_VALUE);
+					jsonMgs.put("dossierId", dossier.getDossierId());
+					jsonMgs.put("fileGroupId", 0);
+					jsonMgs.put("userId", user.getUserId());
+					jsonMgs.put("companyId", user.getCompanyId());
+					jsonMgs.put("groupId", APIUtils.GROUPID);
+					jsonMgs.put("dossierOId", dossier.getOid());
+					jsonMgs.put("govAgencyCode", dossier.getGovAgencyCode());
+					jsonMgs.put("dossierStatus", DossierModel.DOSSIER_STATUS_NEW);
+
+					Message message = new Message();
+					
+					message.put("jsonMsg", jsonMgs.toString());
+					message.put("msgFrom", "outside");
+					
+					MessageBusUtil.sendMessage(
+							"opencps/frontoffice/out/destination", message);
+
+					return Response.status(200).entity(resp.toString()).build();
+				} catch (Exception e) {
+					return Response.status(404).entity(resp.toString()).build();
+				}
+			} else {
+				// Not access resources
+				return Response.status(403).entity(resp.toString()).build();
+			}
+		} else {
+			// Not validate
+			return Response.status(401).entity(resp.toString()).build();
+		}
+	}
 
 	@GET
 	@Path("/dossierparts/servicecode/{serviceid: .*}/govagencycode/{govagencycode: .*}")
@@ -90,7 +161,6 @@ public class OCPSDossierController {
 			// Not validate
 			return Response.status(401).entity(resp.toString()).build();
 		}
-
 	}
 
 	/**
@@ -183,6 +253,8 @@ public class OCPSDossierController {
 
 		return jsDossierParts;
 	}
+	
+	
 
 	@POST
 	@Path("/dossiers")
@@ -191,6 +263,14 @@ public class OCPSDossierController {
 			@Context HttpServletRequest request, String body) {
 
 		JSONObject resp = JSONFactoryUtil.createJSONObject();
+
+		JSONObject input = JSONFactoryUtil.createJSONObject();
+
+		try {
+			input = JSONFactoryUtil.createJSONObject(body);
+		} catch (Exception e) {
+			return Response.status(404).entity(resp.toString()).build();
+		}
 
 		OCPSPermission permit = new OCPSPermission();
 
@@ -202,85 +282,82 @@ public class OCPSDossierController {
 
 		if (Validator.isNotNull(api)) {
 			if (isPermit) {
+
 				ServiceContext context = DossierUtils
 						.getServletContext(request);
 
-				String referenceuid = resp.getString("ReferenceUid");
-				String servicecode = resp.getString("ServiceCode");
-				String servicename = resp.getString("ServiceName");
-				String agencycode = resp.getString("AgencyCode");
-				String agencyname = resp.getString("AgencyName");
-				String applicantname = resp.getString("ApplicantName");
-				String applicantidtype = resp.getString("ApplicantIdType");
-				String applicantidno = resp.getString("ApplicantIdNo");
-				String address = resp.getString("Address");
-				String citycode = resp.getString("CityCode");
-				String cityname = resp.getString("CityName");
-				String districtcode = resp.getString("DistrictCode");
-				String districtname = resp.getString("DistrictName");
-				String wardcode = resp.getString("WardCode");
-				String wardname = resp.getString("WardName");
-				String contacttelno = resp.getString("ContactTelNo");
-				String contactemail = resp.getString("ContactEmail");
-				String dossiernote = resp.getString("DossierNote");
-				String submitdate = resp.getString("SubmitDate");
-				String receivedate = resp.getString("ReceiveDate");
-				String dossierno = resp.getString("DossierNo");
-				String duedate = resp.getString("DueDate");
-				String finishdate = resp.getString("FinishDate");
-				String createdate = resp.getString("CreateDate");
-				String modifieddate = resp.getString("ModifiedDate");
-				String dossierstatus = resp.getString("DossierStatus");
-				String statustext = resp.getString("StatusText");
+				context.setScopeGroupId(APIUtils.GROUPID);
+
+				DossierModel dm = DossierUtils.getDossierModel(input);
+
+				Calendar cal = Calendar.getInstance();
+				int birthDateDay = cal.get(Calendar.DAY_OF_MONTH);
+				int birthDateMonth = cal.get(Calendar.MONTH);
+				int birthDateYear = cal.get(Calendar.YEAR);
 
 				// If actor is Mobile
 				if (auth.isUser(apikey)) {
 					context.setUserId(api.getUserId());
 				}
 
+				// If actor is Agency (WebApp)
 				if (auth.isAgency(apikey)) {
+
 					// Create onlineUser
-					if (Validator.isNotNull(contactemail)) {
+					if (Validator.isNotNull(dm.getContactEmail())) {
 
 						User userOfDossierOnline = APIUtils.getUserByEmail(
-								context.getCompanyId(), contactemail);
+								context.getCompanyId(), dm.getContactEmail());
 
 						long userId = 0;
 
 						if (userOfDossierOnline == null) {
+
 							// create user citizen
-							Calendar cal = Calendar.getInstance();
-							int birthDateDay = cal.get(Calendar.DAY_OF_MONTH);
-							int birthDateMonth = cal.get(Calendar.MONTH);
-							int birthDateYear = cal.get(Calendar.YEAR);
 
 							try {
 								Citizen citizen = CitizenLocalServiceUtil
-										.addCitizen(applicantname,
+										.addCitizen(dm.getApplicantName(),
 												StringPool.BLANK, 0,
 												birthDateDay, birthDateMonth,
-												birthDateYear, address,
-												citycode, districtcode,
-												wardcode, cityname,
-												districtname, wardname,
-												contactemail, StringPool.BLANK,
-												context.getScopeGroupId(),
+												birthDateYear, dm.getAddress(),
+												dm.getCityCode(), dm
+														.getDistrictCode(), dm
+														.getWardCode(),
+												APIUtils.getDictItemName(dm
+														.getCityCode()),
+												APIUtils.getDictItemName(dm
+														.getDistrictCode()),
+												APIUtils.getDictItemName(dm
+														.getWardCode()), dm
+														.getContactEmail(),
+												StringPool.BLANK,
+												APIUtils.GROUPID,
 												StringPool.BLANK,
 												StringPool.BLANK,
 												StringPool.BLANK, null, 0,
 												context);
-								userId = citizen.getMappingUserId();
 
 								if (citizen != null) {
+									User mappingUser = UserLocalServiceUtil
+											.getUser(citizen.getMappingUserId());
+
+									org.opencps.integrate.utils.MessageBusUtil
+											.sendEmailAddressVerification(
+													citizen.getUuid(),
+													mappingUser,
+													dm.getContactEmail(),
+													AccountModel.ACCOUNT_TYPE_CITIZEN,
+													AccountModel.ACCOUNT_REG_TWO_STEP,
+													"khoavd.it@gmail.com",
+													context);
+
 									CitizenLocalServiceUtil
 											.updateStatus(
 													citizen.getCitizenId(),
-													citizen.getUserId(),
-													APIConstants.ACCOUNT_STATUS_APPROVED);
+													context.getUserId(),
+													AccountModel.ACCOUNT_STATUS_APPROVED);
 								}
-								// update default passoword is telNo
-								UserLocalServiceUtil.updatePassword(userId,
-										contacttelno, contacttelno, false);
 
 							} catch (Exception e) {
 								_log.error(e);
@@ -289,17 +366,21 @@ public class OCPSDossierController {
 						} else {
 							userId = userOfDossierOnline.getUserId();
 						}
+						context.setUserId(userId);
 					}
+
 				}
 
 				try {
 					// Create Dossier
+
 					ServiceInfo serviceInfo = ServiceInfoLocalServiceUtil
-							.getServiceInfoByServiceNo(servicecode);
+							.getServiceInfoByServiceNo(dm.getServiceCode());
 
 					ServiceConfig serviceConfig = ServiceConfigLocalServiceUtil
 							.getServiceConfigByG_S_G(context.getScopeGroupId(),
-									serviceInfo.getServiceinfoId(), agencycode);
+									serviceInfo.getServiceinfoId(),
+									dm.getAgencyCode());
 
 					long ownerOrganizationId = 0;
 					long dossierTemplateId = serviceConfig
@@ -311,38 +392,82 @@ public class OCPSDossierController {
 							.getServiceDomainIndex();
 					long govAgencyOrganizationId = serviceConfig
 							.getGovAgencyOrganizationId();
-					String govAgencyName = serviceConfig.getGovAgencyName();
+
 					int serviceMode = 1;
 
 					String serviceAdministrationIndex = serviceConfig
 							.getServiceAdministrationIndex();
+					String dossierDestinationFolder = StringPool.BLANK;
 
-					/*
-					 * DLFolder dossierFolder = DLFolderUtil.getTargetFolder(
-					 * context.getUserId(), context.getScopeGroupId(),
-					 * context.getScopeGroupId(), false, 0,
-					 * dossierDestinationFolder, StringPool.BLANK, false,
-					 * context);
-					 */
-					/*
-					 * Dossier dossier = DossierLocalServiceUtil.addDossier(
-					 * context.getUserId(), ownerOrganizationId,
-					 * dossierTemplateId, templateFileNo, serviceConfigId,
-					 * serviceInfoId, serviceDomainIndex,
-					 * govAgencyOrganizationId, agencycode, agencyname,
-					 * serviceMode, serviceAdministrationIndex, citycode,
-					 * cityname, districtcode, districtname, wardname, wardcode,
-					 * applicantname, applicantidno, address, applicantname,
-					 * contacttelno, contactemail, dossiernote,
-					 * PortletConstants.DOSSIER_SOURCE_DIRECT,
-					 * PortletConstants.DOSSIER_STATUS_NEW,
-					 * dossierFolder.getFolderId(), StringPool.BLANK, context);
-					 */
+					dossierDestinationFolder = DossierUtils
+							.getDossierDestinationFolder(
+									context.getScopeGroupId(), birthDateYear,
+									birthDateMonth, birthDateDay);
+
+					DLFolder dossierFolder = DLFolderUtil.getTargetFolder(
+							context.getUserId(), context.getScopeGroupId(),
+							context.getScopeGroupId(), false, 0,
+							dossierDestinationFolder, StringPool.BLANK, false,
+							context);
+
+					Dossier dossier = DossierUtils.getDossierIdByOid(dm.getReferenceUid());
+
+					if (Validator.isNull(dossier)) {
+						
+						dossier = DossierLocalServiceUtil.addDossier(
+								context.getUserId(), ownerOrganizationId,
+								dossierTemplateId, templateFileNo,
+								serviceConfigId, serviceInfoId,
+								serviceDomainIndex, govAgencyOrganizationId,
+								dm.getAgencyCode(), dm.getAgencyName(),
+								serviceMode, serviceAdministrationIndex,
+								dm.getCityCode(), dm.getCityName(),
+								dm.getDistrictCode(), dm.getDistrictName(),
+								dm.getWardName(), dm.getWardCode(),
+								dm.getApplicantName(), dm.getApplicantIdNo(),
+								dm.getAddress(), dm.getApplicantName(),
+								dm.getContactTelNo(), dm.getContactEmail(),
+								dm.getDossierNote(),
+								DossierModel.DOSSIER_SOURCE_DIRECT,
+								DossierModel.DOSSIER_STATUS_NEW,
+								dossierFolder.getFolderId(), StringPool.BLANK,
+								context);
+						
+						dossier.setOid(dm.getReferenceUid());
+						
+						DossierLocalServiceUtil.updateDossier(dossier);
+						
+						resp.put("Result", "New");
+						resp.put("DossierId", dossier.getDossierId());
+						resp.put("ReferenceId", dossier.getOid());
+						resp.put("AgencyCode", dossier.getGovAgencyCode());
+
+						return Response.status(200).entity(resp.toString())
+								.build();
+
+					} else {
+						resp.put("Result", "Exist");
+						resp.put("DossierId", dossier.getDossierId());
+						resp.put("ReferenceId", dossier.getOid());
+						resp.put("AgencyCode", dossier.getGovAgencyCode());
+						resp.put("ErrorMessage",
+								APIUtils.getLanguageValue("dossier-exist"));
+
+						return Response.status(408).entity(resp.toString())
+								.build();
+
+					}
+
 				} catch (Exception e) {
+					resp.put("Result", "Error");
+
+					resp.put("ErrorMessage",
+							APIUtils.getLanguageValue("create-dossier-error"));
+					
+					_log.error(e);
+					
 					return Response.status(404).entity(resp.toString()).build();
 				}
-
-				return Response.status(200).entity(resp.toString()).build();
 
 			} else {
 				// Not access resources
