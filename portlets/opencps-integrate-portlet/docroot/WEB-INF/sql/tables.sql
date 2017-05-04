fd2a233c4b1b4595c31f34f31deda6ad18c810ea
Nhan Hoang <skynetx002@gmail.com>
2017-05-04 13:47:49 +0700

    generate service

--- portlets/opencps-integrate-portlet/docroot/WEB-INF/sql/tables.sql
+++ portlets/opencps-integrate-portlet/docroot/WEB-INF/sql/tables.sql
@@ -9,3 +9,14 @@ create table opencps_apikey (
 	apiKey VARCHAR(75) null,
 	agency VARCHAR(75) null
 );
+
+create table opencps_apikey_log (
+	id_ LONG not null primary key,
+	companyId LONG,
+	groupId LONG,
+	userId LONG,
+	createDate DATE null,
+	modifiedDate DATE null,
+	actionCode DATE null,
+	apiKey VARCHAR(75) null
+);
