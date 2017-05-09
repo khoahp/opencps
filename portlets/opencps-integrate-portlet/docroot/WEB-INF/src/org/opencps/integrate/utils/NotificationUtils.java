b7c756760deea97489f3ced275d50c12b33a5d46
Khoa Vu Dang <khoavd@fds.vn>
2017-05-09 11:19:08 +0700

    update Notifications

---
+++ portlets/opencps-integrate-portlet/docroot/WEB-INF/src/org/opencps/integrate/utils/NotificationUtils.java
@@ -0,0 +1,31 @@
+package org.opencps.integrate.utils;
+
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+
+public class NotificationUtils {
+	
+	/**
+	 * @param userNotificationEventId
+	 * @return
+	 */
+	public static boolean updateArchived(long userNotificationEventId) {
+
+		UserNotificationEvent userNotificationEvent = null;
+
+		try {
+			userNotificationEvent = UserNotificationEventLocalServiceUtil
+					.getUserNotificationEvent(userNotificationEventId);
+
+			userNotificationEvent.setArchived(true);
+
+			userNotificationEvent = UserNotificationEventLocalServiceUtil
+					.updateUserNotificationEvent(userNotificationEvent);
+			
+			return true;
+		} catch (Exception e) {
+
+		}
+		return false;
+	}
+}
