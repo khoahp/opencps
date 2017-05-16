a1961079843979205d0f5f00a54329a93212c9a4
Khoa Vu Dang <khoavd@fds.vn>
2017-05-16 14:29:37 +0700

    Update API getTechSpec

--- portlets/vr-business-portlet/docroot/WEB-INF/src/com/fds/vrbusiness/api/DictItemController.java
+++ portlets/vr-business-portlet/docroot/WEB-INF/src/com/fds/vrbusiness/api/DictItemController.java
@@ -1,5 +1,27 @@
 package com.fds.vrbusiness.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+
+@Path("/dictitem")
 public class DictItemController {
+	@GET
+	@Path("/alo")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Response alo() {
+
+		JSONObject resp = JSONFactoryUtil.createJSONObject();
+
+		resp.put("Message", "Hello");
+
+		return Response.status(200).entity(resp.toString()).build();
+
+	}
 
 }
--- portlets/vr-business-portlet/docroot/WEB-INF/src/com/fds/vrbusiness/utils/DictItemsUtils.java
+++ portlets/vr-business-portlet/docroot/WEB-INF/src/com/fds/vrbusiness/utils/DictItemsUtils.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import org.opencps.datamgt.model.DictCollection;
 import org.opencps.datamgt.model.DictItem;
-import org.opencps.datamgt.model.DictItemLink;
+import org.opencps.datamgt.model.DictItemType;
 import org.opencps.datamgt.service.DictCollectionLocalServiceUtil;
-import org.opencps.datamgt.service.DictItemLinkLocalServiceUtil;
 import org.opencps.datamgt.service.DictItemLocalServiceUtil;
+import org.opencps.datamgt.service.DictItemTypeLocalServiceUtil;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -28,13 +28,13 @@ public class DictItemsUtils {
 		List<DictItem> dictItems = new ArrayList<>();
 		
 		try {
-			List<DictItemLink> dictItemsLink = DictItemLinkLocalServiceUtil
+			List<DictItemType> dictItemsType = DictItemTypeLocalServiceUtil
 					.getBy_IC_CI_CLI(dictItemLinkedCode, dictCollectionId,
 							dictCollectionLinkedId);
 			
-			for (DictItemLink dil : dictItemsLink) {
+			for (DictItemType dit : dictItemsType) {
 				
-				DictItem di = getDictItem(dil.getDictItemId());
+				DictItem di = getDictItem(dit.getDictItemId());
 				
 				if (Validator.isNotNull(di)) {
 					dictItems.add(di);
